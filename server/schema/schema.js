9bbf8913d7025358e36faf10be86351e21e00436
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2022-06-09 11:39:37 +0100

    Added client and projects to schema

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,5 +1,8 @@
 const {projects, clients} = require ('../sampleData.js')
 
+const Client = require('../models/Client')
+const Project = require('../models/Project')
+
 
 const {GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema, GraphQLList} = require('graphql');
 
@@ -14,7 +17,7 @@ const ProjectType = new GraphQLObjectType({
     client: {
       type: ClientType,
       resolve(parent, args) {
-        return clients.find(client => client.id === parent.clientId)
+        return Client.findById(prent.clientId)
       }
     }
   })
@@ -36,27 +39,27 @@ const RootQuery = new GraphQLObjectType({
     projects: {
       type: new GraphQLList(ProjectType),
       resolve(parent, args) {
-        return projects;
+        return Project.find();
       }
     },
     project: {
       type: ProjectType,
       args: {id: {type: GraphQLID}},
       resolve(parent, args) {
-        return projects.find(project => project.id === args.id)
+        return Project.findById(args.id)
       }
     },
     clients: {
       type: new GraphQLList(ClientType),
       resolve(parent, args) {
-        return clients;
+        return Client.find();
       }
     },
     client: {
       type: ClientType,
       args: {id: {type: GraphQLID}},
       resolve(parent, args) {
-        return clients.find(client => client.id === args.id)
+        return Client.findById(args.id)
       }
     }
   }
