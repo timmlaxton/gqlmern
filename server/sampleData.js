35a80c4dba0ff7555f2414c6503feac5b2c6b31f
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2022-06-09 10:40:16 +0100

    server running

---
+++ server/sampleData.js
@@ -0,0 +1,78 @@
+const projects = [
+  {
+    id: '1',
+    clientId: '1',
+    name: 'eCommerce Website',
+    description:
+      'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.',
+    status: 'In Progress',
+  },
+  {
+    id: '2',
+    clientId: '2',
+    name: 'Dating App',
+    description:
+      'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.',
+    status: 'In Progress',
+  },
+  {
+    id: '3',
+    clientId: '3',
+    name: 'SEO Project',
+    description:
+      'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.',
+    status: 'In Progress',
+  },
+  {
+    id: '4',
+    clientId: '4',
+    name: 'Design Prototype',
+    description:
+      'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.',
+    status: 'Done',
+  },
+  {
+    id: '5',
+    clientId: '5',
+    name: 'Auction Website',
+    description:
+      'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.',
+    status: 'In Progress',
+  },
+];
+
+// Clients
+const clients = [
+  {
+    id: '1',
+    name: 'James Turnbull',
+    email: 'jturnbull@gmail.com',
+    phone: '0141-127-4303',
+  },
+  {
+    id: '2',
+    name: 'Helen Mattheson',
+    email: 'hmattheson@gmail.com',
+    phone: '243-269-3162',
+  },
+  {
+    id: '3',
+    name: 'Margo Felix',
+    email: 'mfelix@gmail.com',
+    phone: '290-889-9711',
+  },
+  {
+    id: '4',
+    name: 'Dominic Peters',
+    email: 'dpeters@gmail.com',
+    phone: '229-899-2003',
+  },
+  {
+    id: '5',
+    name: 'Winona Maple',
+    email: 'wmaple@gmail.com',
+    phone: '229-384-2900',
+  },
+];
+
+module.exports = { projects, clients };
