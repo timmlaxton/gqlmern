6b1747d157c551173fce925044012b941fac2235
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2022-06-17 10:07:27 +0100

    added cache to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,29 @@
 import Header from "./components/Header";
 import Clients from "./components/Clients";
-
-
 import {ApolloProvider, ApolloClient, InMemoryCache} from '@apollo/client'
+
+const cache = new InMemoryCache({
+  typePolicies: {
+    query: {
+      fields:  {
+        clients: {
+        merge(existing, incoming) {
+          return incoming
+        }
+        },
+        projects: {
+          merge(existing, incoming) {
+            return incoming
+          }
+          }
+      }
+    }
+  }
+})
 
 const client = new ApolloClient ({
   uri: 'http://localhost:5000/graphql',
-  cache: new InMemoryCache(),
+  cache,
 })
 
 function App() {
